c52fe51ba72086e947068c2ee7f94e4df58b079c
rpisci2 <rpisci2@students.towson.edu>
2021-04-25 19:53:36 -0700

    User Addition working

--- create.php
+++ create.php
@@ -1,4 +1,44 @@
-<?php include "templates/header.php"; ?><h2>Add a user</h2>
+<?php include "templates/header.php"; ?>
+
+
+<?php
+if (isset($_POST['submit'])) {
+	require "config.php";
+  
+	try {
+	  $connection = new PDO($dsn, $username, $password, $options);
+  
+	  $new_user = array(
+		"firstname" => $_POST['firstname'],
+		"lastname"  => $_POST['lastname'],
+		"email"     => $_POST['email'],
+		"age"       => $_POST['age'],
+		"location"  => $_POST['location']
+	  );
+  
+	  $sql = sprintf(
+  "INSERT INTO %s (%s) values (%s)",
+  "users",
+  implode(", ", array_keys($new_user)),
+  ":" . implode(", :", array_keys($new_user))
+	  );
+  
+	  $statement = $connection->prepare($sql);
+	  $statement->execute($new_user);
+	} catch(PDOException $error) {
+	  echo $sql . "<br>" . $error->getMessage();
+	}
+  
+  }
+?>
+
+<?php require "templates/header.php"; ?>
+
+<?php if (isset($_POST['submit']) && $statement) { ?>
+  > <?php echo $_POST['firstname']; ?> successfully added.
+<?php } ?>
+
+<h2>Add a user</h2>
 <!-- This is the new users page. It allows users to enter in their information and enter a new user into our database-->
     <form method="post">
     	<label for="firstname">First Name</label><br> <!-- label for "input" aka Each label has attribute name and id -->
