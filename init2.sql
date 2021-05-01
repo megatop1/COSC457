663ef026cc943de1d38187a5d0b04da3850d198d
megatop1 <43348736+megatop1@users.noreply.github.com>
2021-05-01 13:03:19 -0400

    Added test data for Client

--- init2.sql
+++ init2.sql
@@ -5,12 +5,23 @@
 DROP TABLE IF EXISTS `Client`;
 
 CREATE TABLE `Client` (
-  `PhoneNumber` int(11) NOT NULL,
-  `FirstName` int(11) NOT NULL,
-  `LastName` int(11) NOT NULL,
+  `PhoneNumber` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `FirstName` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `LastName` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL,
   `ClientID` int(11) NOT NULL,
   PRIMARY KEY (`ClientID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `Client` (`PhoneNumber`, `FirstName`, `LastName`, `ClientID`) VALUES ('(532)474-25', 'Colin', 'Casper', 0);
+INSERT INTO `Client` (`PhoneNumber`, `FirstName`, `LastName`, `ClientID`) VALUES ('370-473-513', 'Jarred', 'Stiedemann', 1);
+INSERT INTO `Client` (`PhoneNumber`, `FirstName`, `LastName`, `ClientID`) VALUES ('975-545-393', 'Tre', 'Glover', 2);
+INSERT INTO `Client` (`PhoneNumber`, `FirstName`, `LastName`, `ClientID`) VALUES ('385-586-695', 'Albina', 'Bogan', 3);
+INSERT INTO `Client` (`PhoneNumber`, `FirstName`, `LastName`, `ClientID`) VALUES ('980-919-926', 'Crystal', 'Greenholt', 4);
+INSERT INTO `Client` (`PhoneNumber`, `FirstName`, `LastName`, `ClientID`) VALUES ('+89(7)38080', 'Chanelle', 'Predovic', 5);
+INSERT INTO `Client` (`PhoneNumber`, `FirstName`, `LastName`, `ClientID`) VALUES ('(629)216-11', 'Myles', 'Upton', 6);
+INSERT INTO `Client` (`PhoneNumber`, `FirstName`, `LastName`, `ClientID`) VALUES ('146-200-601', 'Lelah', 'Funk', 7);
+INSERT INTO `Client` (`PhoneNumber`, `FirstName`, `LastName`, `ClientID`) VALUES ('262-282-969', 'Alphonso', 'Breitenberg', 8);
+INSERT INTO `Client` (`PhoneNumber`, `FirstName`, `LastName`, `ClientID`) VALUES ('984.257.125', 'Felicity', 'Spencer', 9);
 
 
 #
