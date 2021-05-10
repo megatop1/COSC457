032a2b285bc36a322729b0033725975e16a2b5f3
rpisci2 <rpisci2@students.towson.edu>
2021-05-10 01:14:57 -0700

    got data to show in update

--- barber/update-single.php
+++ barber/update-single.php
@@ -20,40 +20,42 @@ if (isset($_POST['submit'])) {
       "Comments"                => $_POST['Comments']
     ];
 
-    $sql = "UPDATE users
-            SET id = :id,
-              firstname = :firstname,
-              lastname = :lastname,
-              email = :email,
-              age = :age,
-              location = :location,
-              date = :date
-            WHERE id = :id";
+    $sql = "UPDATE Appointment
+            SET AppointmentID = :AppointmentID,
+              Date = :Date,
+              AppointmentID = :AppointmentID,
+              DesiredEmployee = :DesiredEmployee,
+              Email = :Email,
+              AppointmentService = :AppointmentService,
+              AppointmentServiceType = :AppointmentServiceType,
+              PaymentType = :PaymentType,
+              Comments = :Comments
+            WHERE AppointmentID = :AppointmentID";
 
   $statement = $connection->prepare($sql);
-  $statement->execute($user);
+  $statement->execute($Appointment);
   } catch(PDOException $error) {
       echo $sql . "<br>" . $error->getMessage();
   }
 }
 
-if (isset($_GET['id'])) {
-  try {
+/* if (isset($_GET['id'])) { /* This line is breaking everything */
+ /* try { */
     $connection = new PDO($dsn, $username, $password, $options);
     $AppointmentID = $_GET['AppointmentID'];
-    $sql = "SELECT * FROM Appointment WHERE AppointmentID = :AppointmentID";
+    $sql = "SELECT * FROM Appointment";
     $statement = $connection->prepare($sql);
     $statement->bindValue(':AppointmentID', $AppointmentID);
     $statement->execute();
 
-    $user = $statement->fetch(PDO::FETCH_ASSOC);
-  } catch(PDOException $error) {
+    $Appointment = $statement->fetch(PDO::FETCH_ASSOC);
+ /* } catch(PDOException $error) {
       echo $sql . "<br>" . $error->getMessage();
-  }
+  } 
 } else {
     echo "Something went wrong!";
     exit;
-}
+}*/
 ?>
 
 <?php require "templates/header.php"; ?>
@@ -65,7 +67,7 @@ if (isset($_GET['id'])) {
 <h2>Edit a user</h2>
 
 <form method="post">
-    <?php foreach ($user as $key => $value) : ?>
+    <?php foreach ($Appointment as $key => $value) : ?>
       <label for="<?php echo $key; ?>"><?php echo ucfirst($key); ?></label>
       <input type="text" name="<?php echo $key; ?>" id="<?php echo $key; ?>" value="<?php echo escape($value); ?>" <?php echo ($key === 'id' ? 'readonly' : null); ?>>
     <?php endforeach; ?>
