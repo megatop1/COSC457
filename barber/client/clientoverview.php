773d0b477a9df5e167af00ba71b80842e7f0fa61
rpisci2 <rpisci2@students.towson.edu>
2021-05-16 17:17:02 -0700

    fixed clientover -> create appointment redirect

--- barber/client/clientoverview.php
+++ barber/client/clientoverview.php
@@ -3,7 +3,7 @@
 <!-- Navbar -->
 <img src="../images/logo.png" alt="Logo Here" style="max-height: 80%; margin-left: 1%; margin-top: 1px">
   <div class="topnav">
-  <a class="active" href="CreateAppointment.php">Create Appointment</a>
+  <a class="active" href="../CreateAppointment.php">Create Appointment</a>
   <a href="../read.php">Scheduled Appointments</a>
   <a href="../update.php">Update Appointment</a>
   <a href="../service/servicelist.php">Services</a>
